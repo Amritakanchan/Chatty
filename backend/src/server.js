077a256dcafd672b566ed72d064937c5d5248680
Amritakanchan <amritakanchan00@gmail.com>
2026-08-24 00:43:26 +0530

    deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import authRoutes from './routes/auth.route.js'
 import msgRoutes from './routes/message.route.js'
 
 import path from 'path'; // doubt
-import { fileURLToPath } from 'url'; // doubt
+import { fileURLToPath } from 'url'; // doubt 
 
 const __filename = fileURLToPath(import.meta.url); // doubt 
 const __dirname = path.dirname(__filename);  // doubt
@@ -19,6 +19,17 @@ console.log('PORT =', PORT);
 app.use('/api/auth', authRoutes);
 
 app.use('/api/message', msgRoutes);
+
+// making ready for deployment
+
+if(process.env.NODE_ENV==='production'){
+    
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get(/(.*)/, (req, res)=> {
+       res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
+    }) 
+}
 
 app.listen(PORT, () => {
     console.log("server started");
