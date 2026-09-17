ae5694f8d2bc96eb63fae78d3eca2b7a7e538928
Amritakanchan <amritakanchan00@gmail.com>
2026-09-17 10:34:02 +0530

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,6 +67,10 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
     const { email, password } = req.body;
 
+    if( !email || !password) {
+        return res.status(400).json({message: "All feilds are required"});
+    }
+
     try {
         const user = await User.findOne({email});
         console.log(user);
